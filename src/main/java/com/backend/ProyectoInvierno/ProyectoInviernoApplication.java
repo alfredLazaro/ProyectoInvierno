b218e272125d8506396d036fa617707f246796c8
maurihv13 <huaytamauricio1@gmail.com>
2023-07-19 17:42:43 -0400

    Configuracion Inicial de Proyecto, con prueba de API de guia

---
+++ src/main/java/com/backend/ProyectoInvierno/ProyectoInviernoApplication.java
@@ -0,0 +1,13 @@
+package com.backend.ProyectoInvierno;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoInviernoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectoInviernoApplication.class, args);
+	}
+
+}
