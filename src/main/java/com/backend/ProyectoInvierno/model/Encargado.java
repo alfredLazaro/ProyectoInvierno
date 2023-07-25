04996255416e9722c28d80d52ac8f61cbb99e347
javierFili <javierfiligranaagreda@gmail.com>
2023-07-25 14:43:36 -0400

    ingresando mejoras de rendimiento

--- src/main/java/com/backend/ProyectoInvierno/model/Encargado.java
+++ src/main/java/com/backend/ProyectoInvierno/model/Encargado.java
@@ -8,99 +8,99 @@ import java.util.List;
 @Entity
 @Table(name = "encargado")
 public class Encargado {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "id_encargado")
-    private long id;
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  @Column(name = "id_encargado")
+  private long id;
 
-    @OneToMany
-    @JoinColumn(name = "id_establecimiento", referencedColumnName = "id_encargado")
-    private List<Establecimiento> establecimientos;
+  @OneToMany
+  @JoinColumn(name = "id_establecimiento", referencedColumnName = "id_encargado")
+  private List<Establecimiento> establecimientos;
 
-    @Column(name = "nombre_encargado")
-    private String nombre;
-    @Column(name = "telefono_encargado")
-    private String telefono;
-    @Column(name = "fecha_registro_encargado")
-    private Date fechaRegistro;
-    @Column(name = "idiomas_encargado")
-    private String idiomas;
-    @Column(name = "correo_encargado")
-    private String correo;
-    @Column(name = "contrasena_encargado")
-    private String contrasenia;
-    @Column(name = "fecha_nacimiento_encargado")
-    private Date fechaNacimiento;
+  @Column(name = "nombre_encargado")
+  private String nombre;
+  @Column(name = "telefono_encargado")
+  private String telefono;
+  @Column(name = "fecha_registro_encargado")
+  private Date fechaRegistro;
+  @Column(name = "idiomas_encargado")
+  private String idiomas;
+  @Column(name = "correo_encargado")
+  private String correo;
+  @Column(name = "contrasena_encargado")
+  private String contrasenia;
+  @Column(name = "fecha_nacimiento_encargado")
+  private Date fechaNacimiento;
 
-    public long getId() {
-        return id;
-    }
+  public long getId() {
+    return id;
+  }
 
-    public void setId(long id) {
-        this.id = id;
-    }
+  public void setId(long id) {
+    this.id = id;
+  }
 
-    public List<Establecimiento> getEstablecimientos() {
-        return establecimientos;
-    }
+  public List<Establecimiento> getEstablecimientos() {
+    return establecimientos;
+  }
 
-    public void setEstablecimientos(List<Establecimiento> establecimientos) {
-        this.establecimientos = establecimientos;
-    }
+  public void setEstablecimientos(List<Establecimiento> establecimientos) {
+    this.establecimientos = establecimientos;
+  }
 
-    public String getNombre() {
-        return nombre;
-    }
+  public String getNombre() {
+    return nombre;
+  }
 
-    public void setNombre(String nombre) {
-        this.nombre = nombre;
-    }
+  public void setNombre(String nombre) {
+    this.nombre = nombre;
+  }
 
-    public String getTelefono() {
-        return telefono;
-    }
+  public String getTelefono() {
+    return telefono;
+  }
 
-    public void setTelefono(String telefono) {
-        this.telefono = telefono;
-    }
+  public void setTelefono(String telefono) {
+    this.telefono = telefono;
+  }
 
-    public Date getFechaRegistro() {
-        return fechaRegistro;
-    }
+  public Date getFechaRegistro() {
+    return fechaRegistro;
+  }
 
-    public void setFechaRegistro(Date fechaRegistro) {
-        this.fechaRegistro = fechaRegistro;
-    }
+  public void setFechaRegistro(Date fechaRegistro) {
+    this.fechaRegistro = fechaRegistro;
+  }
 
-    public String getIdiomas() {
-        return idiomas;
-    }
+  public String getIdiomas() {
+    return idiomas;
+  }
 
-    public void setIdiomas(String idiomas) {
-        this.idiomas = idiomas;
-    }
+  public void setIdiomas(String idiomas) {
+    this.idiomas = idiomas;
+  }
 
-    public String getCorreo() {
-        return correo;
-    }
+  public String getCorreo() {
+    return correo;
+  }
 
-    public void setCorreo(String correo) {
-        this.correo = correo;
-    }
+  public void setCorreo(String correo) {
+    this.correo = correo;
+  }
 
-    public String getContrasenia() {
-        return contrasenia;
-    }
+  public String getContrasenia() {
+    return contrasenia;
+  }
 
-    public void setContrasenia(String contrasenia) {
-        this.contrasenia = contrasenia;
-    }
+  public void setContrasenia(String contrasenia) {
+    this.contrasenia = contrasenia;
+  }
 
-    public Date getFechaNacimiento() {
-        return fechaNacimiento;
-    }
+  public Date getFechaNacimiento() {
+    return fechaNacimiento;
+  }
 
-    public void setFechaNacimiento(Date fechaNacimiento) {
-        this.fechaNacimiento = fechaNacimiento;
-    }
+  public void setFechaNacimiento(Date fechaNacimiento) {
+    this.fechaNacimiento = fechaNacimiento;
+  }
 }
