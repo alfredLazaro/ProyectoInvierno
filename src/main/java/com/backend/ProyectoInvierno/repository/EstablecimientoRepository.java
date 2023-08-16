9c0c1f4be0a219033980118bec4a3be14ec803d9
maurihv13 <huaytamauricio1@gmail.com>
2023-08-16 08:44:33 -0400

    Eliminacion de elementos innecesarios que estaban en español

--- src/main/java/com/backend/ProyectoInvierno/repository/EstablecimientoRepository.java
+++
@@ -1,9 +0,0 @@
-package com.backend.ProyectoInvierno.repository;
-
-import com.backend.ProyectoInvierno.model.Establecimiento;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface EstablecimientoRepository extends JpaRepository<Establecimiento, Long> {
-}
